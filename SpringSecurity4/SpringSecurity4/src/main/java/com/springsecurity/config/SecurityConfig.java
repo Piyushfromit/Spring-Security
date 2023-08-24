f6fdddb0bfc207e4a937bf4aa045b24af7404fec
piyush <piyush.kumar@w2c.co.in>
2023-08-24 15:35:40 +0530

    jkhajk

---
+++ SpringSecurity4/SpringSecurity4/src/main/java/com/springsecurity/config/SecurityConfig.java
@@ -0,0 +1,54 @@
+package com.springsecurity.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class SecurityConfig {
+	
+	//authentication
+	public UserDetailsService userDetailService()
+	{
+		
+		return new UserInfoUserDetailService();
+	}
+	
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+		return http.csrf().disable()
+		.authorizeHttpRequests()
+		.antMatchers("/user","/new").permitAll()
+		.and()
+		.authorizeHttpRequests()
+		.antMatchers("/admin/**")
+		.authenticated()
+		.and()
+		.formLogin().and()
+		.build();
+		
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder()
+	{
+		return new BCryptPasswordEncoder();
+	}
+	/*
+	 * @Bean public AuthenticationProvider authenticationProvider() {
+	 * DaoAuthenticationProvider authenticationProvider = new
+	 * DaoAuthenticationProvider();
+	 * authenticationProvider.setUserDetailsService(userDetailService());
+	 * authenticationProvider.setPasswordEncoder(passwordEncoder()); return
+	 * authenticationProvider; }
+	 */
+}
