5ace4bb333b00b744f1bd44b1c51080bf8561858
piyush_kumar <piyush.kumar@sbsind.in>
2024-04-16 18:27:13 +0530

    hgasda

--- springsecuritybyeazybytes_3/src/main/java/com/springsecuritybyeazybytes/securityconfig/ProjectSecurityConfig.java
+++ springsecuritybyeazybytes_3/src/main/java/com/springsecuritybyeazybytes/securityconfig/ProjectSecurityConfig.java
@@ -1,10 +1,16 @@
 package com.springsecuritybyeazybytes.securityconfig;
 
 
+import org.springframework.cglib.proxy.NoOp;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -12,58 +18,61 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 @EnableWebSecurity
 public class ProjectSecurityConfig {
+
+    // doc Link :     https://docs.spring.io/spring-security/reference/servlet/configuration/java.html
+
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
 
-        //  Custom Security Configuration
-        http.authorizeHttpRequests(authorize -> authorize
+         http.authorizeHttpRequests(authorize -> authorize
                .requestMatchers("/myAccount", "/myBalance", "/myCards", "/myLone").authenticated()
                .requestMatchers("/welcome", "/contactUs", "/notices").permitAll()
                .anyRequest().permitAll()
         )
         .formLogin(withDefaults())
         .httpBasic(withDefaults());
-
-
-        //  Configuration to deny All the request
-        /*http.authorizeHttpRequests(authorize -> authorize
-                .anyRequest().denyAll()
-        )
-        .formLogin(withDefaults())
-        .httpBasic(withDefaults());*/
-
-
-        //  Configuration to permit All the request
-       /* http.authorizeHttpRequests(authorize -> authorize
-               .anyRequest().permitAll()
-        )
-        .formLogin(withDefaults())
-        .httpBasic(withDefaults());*/
-
-
-        return  http.build();
+     return  http.build();
     }
+
+    // Approach 1 for InMemoryUserDetailsManager
+
+//    @Bean
+//    public InMemoryUserDetailsManager userDetailsService1() throws Exception {
+//
+//        UserDetails users1 = User.withDefaultPasswordEncoder()
+//                .username("admin")
+//                .password("123456")
+//                .roles("admin")
+//                .build();
+//        UserDetails users2 = User.withDefaultPasswordEncoder()
+//                .username("user")
+//                .password("123456")
+//                .roles("read")
+//                .build();
+//
+//        return new InMemoryUserDetailsManager(users1, users2);
+//    }
+
+    // Approach 2 for InMemoryUserDetailsManager
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService2() throws Exception {
+
+        InMemoryUserDetailsManager inMemoryUserDetailsManager = new InMemoryUserDetailsManager();
+
+        UserDetails user= User.withUsername("admin").password("12345678").authorities("USER").build();
+        UserDetails admin= User.withUsername("user").password("12345678").authorities("USER","ADMIN").build();
+        inMemoryUserDetailsManager.createUser(user);
+        inMemoryUserDetailsManager.createUser(admin);
+        return inMemoryUserDetailsManager;
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+
 }
 
 
-// doc Link :     https://docs.spring.io/spring-security/reference/servlet/configuration/java.html
 
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityConfig {
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-//
-//        httpSecurity.authorizeHttpRequests(request -> request
-//                        .requestMatchers("/admin/**").authenticated()
-//                        .anyRequest().permitAll()
-//                )
-//                .formLogin(formLogin -> formLogin .defaultSuccessUrl("/admin/dashboard", true) )
-//                .logout(logout ->  logout  .permitAll())
-//                .csrf(csrf -> csrf.disable());
-//        ;
-//
-//        return  httpSecurity.build();
-//    }
-//}
+
