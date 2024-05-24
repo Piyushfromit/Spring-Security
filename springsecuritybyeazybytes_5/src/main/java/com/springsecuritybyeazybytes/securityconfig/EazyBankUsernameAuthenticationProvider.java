cde99f898a56eb0b6fbf0a7c9e4984c90f9cb0e1
piyushfromit <piyushfrommasai@gmail.com>
2024-05-24 07:19:08 +0530

    section 5

--- springsecuritybyeazybytes_5/src/main/java/com/springsecuritybyeazybytes/securityconfig/EazyBankUsernameAuthenticationProvider.java
+++ springsecuritybyeazybytes_5/src/main/java/com/springsecuritybyeazybytes/securityconfig/EazyBankUsernameAuthenticationProvider.java
@@ -1,14 +1,25 @@
 package com.springsecuritybyeazybytes.securityconfig;
 
 
+import com.springsecuritybyeazybytes.model.Customer;
 import com.springsecuritybyeazybytes.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
 public class EazyBankUsernameAuthenticationProvider implements AuthenticationProvider {
 
     @Autowired
@@ -18,12 +29,27 @@ public class EazyBankUsernameAuthenticationProvider implements AuthenticationPro
     private PasswordEncoder passwordEncoder;
 
 
-
-
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
-    }
+
+        String username  = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customer = customerRepository.findByEmail(username);
+
+        if(customer.size() > 0){
+            if(passwordEncoder.matches(pwd, customer.get(0).getPwd())){
+                List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+                grantedAuthorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+                User user = new User(username, pwd, grantedAuthorities);
+                return new UsernamePasswordAuthenticationToken(user, pwd, grantedAuthorities);
+            }else{
+                throw new BadCredentialsException("Invalid password");
+            }
+
+        }else{
+           throw new UsernameNotFoundException("User details not found for the user:"  + username );
+        }
+   }
 
 
     @Override
