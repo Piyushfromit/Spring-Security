18742f1bc6ec39820d41d1e3fc90f962342e5d71
piyushfromit <piyushfrommasai@gmail.com>
2024-06-04 07:26:21 +0530

    jhgdhq

---
+++ springsecuritybyeazybytes_7/springsecuritybyeazybytes_7/src/main/java/com/springsecuritybyeazybytes/model/Authority.java
@@ -0,0 +1,23 @@
+package com.springsecuritybyeazybytes.model;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.GenericGenerator;
+
+@Entity
+@Table(name = "authorities")
+public class Authority {
+
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO,generator="native")
+    @GenericGenerator(name = "native",strategy = "native")
+    private Long id;
+
+    private String name;
+
+
+    @ManyToMany
+    @JoinColumn(name = "customer_id")
+    private Customer customer;
+}
