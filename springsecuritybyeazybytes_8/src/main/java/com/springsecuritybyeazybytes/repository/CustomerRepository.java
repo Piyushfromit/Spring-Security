96e6852e8dc37a71bdf71cd4b20f0bc11f138e23
piyushfromit <piyushfrommasai@gmail.com>
2024-06-10 00:37:06 +0530

    88888

---
+++ springsecuritybyeazybytes_8/src/main/java/com/springsecuritybyeazybytes/repository/CustomerRepository.java
@@ -0,0 +1,16 @@
+package com.springsecuritybyeazybytes.repository;
+
+
+import com.springsecuritybyeazybytes.model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CustomerRepository extends JpaRepository<Customer, Long> {
+
+    List<Customer> findByEmail(String email);
+    
+}
---
+++ springsecuritybyeazybytes_8/src/main/java/com/springsecuritybyeazybytes/securityconfig/EazyBankUsernameAuthenticationProvider.java
@@ -0,0 +1,72 @@
+package com.springsecuritybyeazybytes.securityconfig;
+
+
+import com.springsecuritybyeazybytes.model.Authority;
+import com.springsecuritybyeazybytes.model.Customer;
+import com.springsecuritybyeazybytes.repository.CustomerRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Component
+public class EazyBankUsernameAuthenticationProvider implements AuthenticationProvider {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+
+        String username  = authentication.getName();
+        String pwd = authentication.getCredentials().toString();
+        List<Customer> customer = customerRepository.findByEmail(username);
+
+        if(customer.size() > 0){
+            if(passwordEncoder.matches(pwd, customer.get(0).getPwd())){
+                //List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+                //grantedAuthorities.add(new SimpleGrantedAuthority(customer.get(0).getRole()));
+                return new UsernamePasswordAuthenticationToken(username, pwd, getGrantedAuthoities(customer.get(0).getAuthorities()));
+            }else{
+                throw new BadCredentialsException("Invalid password");
+            }
+
+        }else{
+           throw new UsernameNotFoundException("User details not found for the user:"  + username );
+        }
+   }
+
+
+   private List<GrantedAuthority> getGrantedAuthoities(Set<Authority> authorities){
+        List<GrantedAuthority> grantedAuthorities = new ArrayList<>();
+        for(Authority authority: authorities){
+            grantedAuthorities.add(new SimpleGrantedAuthority(authority.getName()));
+        }
+        return grantedAuthorities;
+   }
+
+
+
+    @Override
+    public boolean supports(Class<?> authentication) {
+        return (UsernamePasswordAuthenticationToken.class.isAssignableFrom(authentication));
+    }
+
+
+
+}
